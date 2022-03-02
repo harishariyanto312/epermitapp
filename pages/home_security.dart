46b7e00d74fc8143fe13e454da457d96825cc6de
Haris Hariyanto <harishariyanto312@gmail.com>
2022-03-02 16:03:17 +0700

    Mboh

--- pages/home_security.dart
+++ pages/home_security.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
 import 'package:epermits/pages/home.dart';
 import 'package:epermits/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutx/flutx.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import './scanner.dart';
+import '../network/sanctum_api.dart';
 
 class HomeSecurity extends StatefulWidget {
   final Function() logoutHandler;
@@ -15,10 +19,23 @@ class HomeSecurity extends StatefulWidget {
 }
 
 class _HomeSecurityState extends State<HomeSecurity> {
+  final inputPermitID = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  var isLoading = false;
+
+  showSpinner() {
+    return Center(
+      child: SpinKitDoubleBounce(
+        color: AppTheme.theme.colorScheme.primary,
+        size: 50,
+      ),
+    );
+  }
+
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: _generateFab(),
+      floatingActionButton: isLoading ? null : _generateFab(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       appBar: AppBar(
         elevation: 0,
@@ -53,16 +70,102 @@ class _HomeSecurityState extends State<HomeSecurity> {
           ),
         ],
       ),
+      body: isLoading ? showSpinner() : _securityMainScreen(),
+    );
+  }
+
+  _securityMainScreen() {
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: FxSpacing.nTop(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 12),
+              child: FxText.sh1('Masukkan Nomor Surat Izin Keluar', fontWeight: 600,),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Nomor Surat',
+                border: AppTheme.theme.inputDecorationTheme.border,
+                enabledBorder: AppTheme.theme.inputDecorationTheme.border,
+                focusedBorder: AppTheme.theme.inputDecorationTheme.focusedBorder,
+                prefixIcon: Icon(
+                  MdiIcons.numeric,
+                  size: 24,
+                ),
+              ),
+              controller: inputPermitID,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nomor tidak boleh kosong';
+                }
+                return null;
+              },
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 8,),
+              alignment: Alignment.center,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppTheme.theme.colorScheme.primary.withAlpha(28),
+                      blurRadius: 4,
+                      offset: Offset(0 ,3),
+                    ),
+                  ],
+                ),
+                child: ElevatedButton(
+                  onPressed: _checkPermit,
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(FxSpacing.xy(16, 0)),
+                  ),
+                  child: FxText.button(
+                    'CEK',
+                    fontWeight: 700,
+                    color: AppTheme.theme.colorScheme.onPrimary,
+                    letterSpacing: 0.5,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
   _generateFab() {
     return FloatingActionButton.extended(
-      onPressed: () {
-        Navigator.push(
+      onPressed: () async {
+        var result = await Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => Scanner()),
         );
+        if (result != null) {
+          setState(() {
+            isLoading = true;
+          });
+
+          var permitID = result;
+          var permitData = await _getPermit(permitID);
+
+          setState(() {
+            isLoading = false;
+          });
+
+          if (permitData['errors'] == null) {
+            print('OK Found');
+          }
+          else {
+            _showDialog();
+          }
+        }
       },
       elevation: 2,
       label: FxText.sh2(
@@ -72,6 +175,112 @@ class _HomeSecurityState extends State<HomeSecurity> {
         letterSpacing: 0.3,
       ),
       icon: Icon(MdiIcons.qrcodeScan),
+    );
+  }
+
+  _getPermit(permitID) async {
+    var res = await SanctumApi().sendGet(
+      apiURL: 'permits/' + permitID.toString(),
+      additionalHeaders: {},
+      withToken: true,
+    );
+    var body = jsonDecode(res.body);
+    return body;
+  }
+
+  _checkPermit() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        isLoading = true;
+      });
+
+      var permitID = inputPermitID.text;
+      var permitData = await _getPermit(permitID);
+
+      setState(() {
+        isLoading = false;
+      });
+
+      if (permitData['errors'] == null) {
+        print('OK Found');
+      }
+      else {
+        _showDialog();
+      }
+    }
+  }
+
+  _showDialog() {
+    showDialog(
+      context: context, 
+      builder: (BuildContext context) {
+        return Dialog(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+            decoration: BoxDecoration(
+              color: AppTheme.theme.backgroundColor,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10,
+                  offset: Offset(0, 10),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Center(
+                    child: Icon(
+                      MdiIcons.fileQuestion,
+                      size: 40,
+                      color: AppTheme.theme.colorScheme.onBackground.withAlpha(220),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 16),
+                  child: Center(
+                    child: FxText.sh1('Izin Keluar Tidak Ditemukan!', fontWeight: 700,),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 16),
+                  child: Center(
+                    child: FxText.caption(
+                      'Periksa kembali nomor surat izin keluar',
+                      fontWeight: 500,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 16),
+                  child: Center(
+                    child: FxButton(
+                      backgroundColor: AppTheme.theme.colorScheme.background,
+                      elevation: 2,
+                      borderRadiusAll: 4,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: FxText.caption(
+                        'OK',
+                        fontWeight: 600,
+                        letterSpacing: 0.3,
+                        color: AppTheme.theme.colorScheme.onPrimary,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- pages/scanner.dart
+++ pages/scanner.dart
@@ -1,6 +1,7 @@
 import 'package:epermits/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutx/flutx.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import './home_security.dart';
 
@@ -13,6 +14,7 @@ class _ScannerState extends State<Scanner> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   QRViewController? controller;
   Barcode? result;
+  bool isFlashlightOn = false;
 
   @override 
   Widget build(BuildContext context) {
@@ -32,29 +34,82 @@ class _ScannerState extends State<Scanner> {
                 borderRadius: 10,
                 borderLength: 30,
                 borderWidth: 10,
-                cutOutSize: MediaQuery.of(context).size.width / 1.5,
+                cutOutSize: MediaQuery.of(context).size.width / 1.2,
               ),
             ),
           ),
           Expanded(
             flex: 1,
-            child: (result != null)
-              ? Text('Data ${result?.code}')
-              : Text('Scan a code'),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ClipOval(
+                    child: Material(
+                      color: AppTheme.theme.colorScheme.background,
+                      child: InkWell(
+                        splashColor: Colors.white,
+                        child: SizedBox(
+                          width: 48,
+                          height: 48,
+                          child: Icon(Icons.flip_camera_android, color: Colors.white,),
+                        ),
+                        onTap: () async {
+                          await controller?.flipCamera();
+                          setState(() {});
+                        },
+                      ),
+                    ),
+                  ),
+
+                  ClipOval(
+                    child: Material(
+                      color: AppTheme.theme.colorScheme.background,
+                      child: InkWell(
+                        splashColor: Colors.white,
+                        child: SizedBox(
+                          width: 48,
+                          height: 48,
+                          child: Icon(isFlashlightOn ? Icons.flashlight_off : Icons.flashlight_on, color: Colors.white,),
+                        ),
+                        onTap: () async {
+                          await controller?.toggleFlash();
+                          setState(() {
+                            isFlashlightOn = !isFlashlightOn;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
+      ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: AppTheme.theme.colorScheme.background,
+        title: FxText.sh1('Scan QR', fontWeight: 600, color: AppTheme.theme.colorScheme.onPrimary,),
+        iconTheme: IconThemeData(
+          color: AppTheme.theme.colorScheme.onPrimary,
+        ),
       ),
     );
   }
 
   void _onQRViewCreated(QRViewController controller) {
     this.controller = controller;
+    bool scanned = false;
     controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
-      });
-      controller.pauseCamera();
-      Navigator.pop(context);
+      if (!scanned) {
+        scanned = true;
+        setState(() {
+          result = scanData;
+        });
+        controller.pauseCamera();
+        Navigator.pop(context, result?.code);
+      }
     });
   }
 }
